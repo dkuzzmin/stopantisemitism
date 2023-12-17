25326cb5e632d38749964540d6de45de01fb2600
DK <skyhome@MacBook-Pro-DK.local>
2023-12-17 12:16:50 +0300

    your comment

---
+++ main.py
@@ -0,0 +1,51 @@
+
+SQL table
+
+
+pattern word 
+
+class patternword (word, anitisem)
+    word = ''
+    antisem = True
+    posts_base
+
+
+def  analyse text
+
+
+
+
+# sql online database
+
+# set databases:
+# - antisem sequnces
+# - pro sequunses
+# - main input base of publications (posts)
+# - univers in watch list
+# ?table of json’s files
+
+# !find main metodology (finding hate speech)
+
+# py-file:
+# - connection to db
+# - db link one-one
+# - functions(features) in exter file (internal modul)
+# - sql extract
+# - using some external api
+# - stat modul python
+# - word analisys modul
+# - visual module
+# - ? class ??? how to use?
+# - ? user manager??
+
+
+# output:
+# - dashboard (imafe file?)
+# - html file on some server online
+# - like telegram channel everyday stats
+
+
+
+
+
+
